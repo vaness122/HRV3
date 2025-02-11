50b0e7327b069f37cd068ce4514deafd02147c68
Charlie <Charlie@DESKTOP-AOJDK8M>
2025-02-11 22:37:25 +0800

    applied tailwind css

--- hr-react-app/src/components/Register.jsx
+++ hr-react-app/src/components/Register.jsx
@@ -28,31 +28,54 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Username:</label>
+    <section class="bg-gray-50 dark:bg-gray-900">
+       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+       <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+    <form  class="space-y-4 md:space-y-2"  onSubmit={handleSubmit}>
+
+    <div>
+      <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username:</label>
       <input
-        type="text"
+        type="text" 
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value={username}
         onChange={(event) => setUsername(event.target.value)}
       />
+      </div>
+
       <br />
-      <label>Password:</label>
+
+      <div>
+      <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password:</label>
       <input
         type="password"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value={password}
         onChange={(event) => setPassword(event.target.value)}
       />
+    </div>
+
       <br />
-      <label>Confirm Password:</label>
+
+      <div>
+      <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password:</label>
       <input
         type="password"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value={confirmPassword}
         onChange={(event) => setConfirmPassword(event.target.value)}
       />
+      </div>
+      
       <br />
-      <button type="submit">Register</button>
+      <button  
+      type="submit"  
+      class="dark:focus:ring-primary-800">Register</button>
       {error && <p style={{ color: 'red' }}>{error}</p>}
     </form>
+    </div>
+    </div>
+    </section>
   );
 };
 
