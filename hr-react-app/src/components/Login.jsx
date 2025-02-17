3c6bc7e89fba9c1443ac827f7736fcbfd6bdcfd4
Charlie <Charlie@DESKTOP-AOJDK8M>
2025-02-17 13:36:53 +0800

    mod con

--- hr-react-app/src/components/Login.jsx
+++ hr-react-app/src/components/Login.jsx
@@ -5,9 +5,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const Login = ({}) => {
 
-import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
