46474e4165b78f30ff9fd481dc9c70236629712b
Charlie <Charlie@DESKTOP-AOJDK8M>
2025-02-23 23:26:03 +0800

    modified sidebar

--- hr-react-app/src/components/Sidebar.jsx
+++ hr-react-app/src/components/Sidebar.jsx
@@ -13,7 +13,6 @@ const Sidebar = () => {
 
   return (
     <div
-<<<<<<< HEAD
      
       style={{
         // Adjust width based on collapse state
@@ -21,33 +20,17 @@ const Sidebar = () => {
         backgroundColor: 'gray',
         padding: '75px',
        
-=======
-      className="sidebar"
-      style={{
-        width: '250px', // Fixed width of the sidebar
-        height: '100vh',
-        backgroundColor: 'gray',
-        padding: '20px',
->>>>>>> 4fc4e583ad7e7e56d961add70852947468bf468e
         position: 'fixed', // Ensure sidebar stays fixed on the left
         top: 0,
         bottom: 0,
         left :0
       }}
     >
-<<<<<<< HEAD
       <button onClick={toggleSidebar} className="toggle-btn" style={{ position: 'absolute', top: '20px', right: '-30px' }}>
         {}
       </button>
       <h3 style={{ display: isCollapsed ? 'none' : 'block' }}>Dashboard</h3>
       <ul>
-=======
-      {/* Sidebar Header */}
-      <h3>Dashboard</h3>
-
-      {/* Sidebar Links */}
-      <ul className="sidebar-links">
->>>>>>> 4fc4e583ad7e7e56d961add70852947468bf468e
         <li>
           <Link to="/profile">Profile</Link>
         </li>
